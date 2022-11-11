cd918e3c8359d973186f373686e694543c80599a
Wss <Wss@qq.com>
2022-11-11 21:15:41 +0800

    master 提交5

--- src/main/java/com/wl/gitTest.java
+++ src/main/java/com/wl/gitTest.java
@@ -8,5 +8,6 @@ public class gitTest {
         System.out.println("进行合并！");
         System.out.println("master-test");
         System.out.println("hot-fix test！");
+        System.out.println("在修改");
     }
 }
