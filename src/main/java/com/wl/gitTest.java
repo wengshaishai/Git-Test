f4d22902fc0cb11495b1ccc1506b39336058d2b3
Wss <Wss@qq.com>
2022-11-11 19:01:16 +0800

    提交2

--- src/main/java/com/wl/gitTest.java
+++ src/main/java/com/wl/gitTest.java
@@ -3,5 +3,6 @@ package com.wl;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!!!");
+        System.out.println("hello 我修改了！");
     }
 }
